b75dc5fd349909c722464729538a416ee2c54ac9
Lou Amadio <lamadio@microsoft.com>
2018-05-02 16:02:03 -0700

    Add Windows UWP API and sample app.
    This change adds a UWP API and sample app for the Misty API surface.

---
+++ API_Wrappers/UWP/MistyAPI/MistyAPI/Misty.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Security.Credentials;
+using Windows.Security.Cryptography;
+using Windows.Storage.Streams;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+using Windows.Web.Http.Headers;
+using Windows.UI;
+
+namespace MistyAPI
+{
+    public enum Moods
+    {
+        Sad,
+        Angry,
+        Groggy,
+        Confused,
+        Content,
+        Concerned,
+        Unamused,
+        Happy,
+        Love
+    };
+
+    public sealed class Misty
+    {
+        internal struct Mood
+        {
+            public Color ledColor;
+            public int valence;
+            public int arousal;
+            public int dominance;
+        };
+
+        internal readonly Dictionary<Moods, Mood> moods = new Dictionary<Moods, Mood>()
+        {
+            { Moods.Sad, new Mood () { ledColor = ColorHelper.FromArgb(255, 15, 160, 250), valence = -1, arousal = -1, dominance = 0 } },
+            { Moods.Angry,  new Mood() {ledColor = ColorHelper.FromArgb(255, 160, 20, 20), valence = -1, arousal = 1, dominance = 0 } },
+            { Moods.Groggy, new Mood() {ledColor = ColorHelper.FromArgb(255, 210, 215, 220), valence = 0, arousal = -1, dominance = 0 } },
+            { Moods.Confused, new Mood() {ledColor = ColorHelper.FromArgb(255, 130, 15, 250), valence = 1, arousal = 0, dominance = 0 } },
+            { Moods.Content, new Mood() {ledColor = ColorHelper.FromArgb(255, 15, 190, 70), valence = 0, arousal = 0, dominance = 0 } },
+            { Moods.Concerned, new Mood() {ledColor = ColorHelper.FromArgb(255, 50, 115, 180), valence = 0, arousal = 1, dominance = 0 } },
+            { Moods.Unamused, new Mood() {ledColor = ColorHelper.FromArgb(255, 185, 185, 130), valence = 1, arousal = -1, dominance = 0 } },
+            { Moods.Happy, new Mood() {ledColor = ColorHelper.FromArgb(255, 250, 245, 15), valence = 1, arousal = 1, dominance = 0 } },
+            { Moods.Love, new Mood() {ledColor = ColorHelper.FromArgb(255, 240, 90, 90), valence = 1, arousal = 1, dominance = 1 } },
+        };
+
+        const string kEndpointDeviceInfo = "/api/info/device";
+        const string kEndpointChangeEyes = "/api/ChangeEyes";
+        const string kEndpointChangeLED = "/api/ChangeLED";
+
+        const string kTimeout = "500"; // ms
+
+        HttpClient httpClient;
+        string ip;
+        int port;
+
+        private struct RemoteServerStringStatus
+        {
+            public HttpStatusCode statusCode;
+            public string response;
+        }
+
+        private struct RemoteServerJsonStatus
+        {
+            public HttpStatusCode statusCode;
+            public JsonObject json;
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return httpClient != null;
+            }
+        }
+
+        public IAsyncOperation<HttpStatusCode> ConnectAsync(string ip, int port)
+        {
+            return ConnectAsyncTask(ip, port).AsAsyncOperation<HttpStatusCode>();
+        }
+
+        public IAsyncOperation<HttpStatusCode> SetMoodAsync(Moods mood)
+        {
+            return setMoodTask(mood).AsAsyncOperation<HttpStatusCode>();
+        }
+
+        internal async Task<HttpStatusCode> ConnectAsyncTask(string ip, int port)
+        {
+            disconnect();
+
+            var myFilter = new HttpBaseProtocolFilter();
+            myFilter.AllowUI = false;
+            myFilter.AllowAutoRedirect = false;
+            //myFilter.ServerCredential = new PasswordCredential("root", username, password);
+
+            httpClient = new HttpClient(myFilter);
+            this.ip = ip;
+            this.port = port;
+
+            var jsonResp = await requestJson(kEndpointDeviceInfo);
+            if (jsonResp.statusCode != HttpStatusCode.Ok)
+            {
+                return jsonResp.statusCode;
+            }
+
+            var information = jsonResp.json.GetObject();
+            // todo: parse
+
+            return HttpStatusCode.Ok;
+        }
+
+        public void disconnect()
+        {
+            httpClient?.Dispose();
+            httpClient = null;
+        }
+
+        internal async Task<HttpStatusCode> setMoodTask(Moods mood)
+        {
+            return await setLEDTask(moods[mood].ledColor);
+        }
+
+        internal async Task<HttpStatusCode> setLEDTask(Color color)
+        {
+            var builder = new UriBuilder();
+            builder.Host = ip;
+            builder.Port = port;
+            builder.Path = kEndpointChangeLED;
+
+            JsonObject obj = new JsonObject();
+            obj.SetNamedValue("red", JsonValue.CreateNumberValue(color.R));
+            obj.SetNamedValue("green", JsonValue.CreateNumberValue(color.G));
+            obj.SetNamedValue("blue", JsonValue.CreateNumberValue(color.B));
+
+            var content = new HttpStringContent(obj.ToString());
+            var response = await httpClient.PostAsync(builder.Uri, content);
+
+            return response.StatusCode;
+        }
+
+
+
+        private async Task<RemoteServerStringStatus> requestString(string endpoint)
+        {
+            var resp = new RemoteServerStringStatus();
+
+            var builder = new UriBuilder();
+            builder.Host = ip;
+            builder.Port = port;
+            builder.Path = endpoint;
+
+            var response = await httpClient.GetAsync(builder.Uri);
+            resp.statusCode = response.StatusCode;
+            resp.response = await response.Content.ReadAsStringAsync();
+
+            return resp;
+        }
+
+        private async Task<RemoteServerJsonStatus> requestJson(string endpoint)
+        {
+            var resp = new RemoteServerJsonStatus();
+            var stringResp = await requestString(endpoint);
+            resp.statusCode = stringResp.statusCode;
+            if (stringResp.statusCode == HttpStatusCode.Ok)
+            {
+                JsonObject.TryParse(stringResp.response, out resp.json);
+            }
+
+            return resp;
+        }
+    }
+}
---
+++ Sample Code/UWPApp/APIExplorer/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MistyAPI;
+using Windows.System;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace APIExplorer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const int kWebSocketPort = 80;
+
+        Misty currentMisty = new Misty();
+
+        MistyFinder mistyFinder = new MistyFinder();
+        const string kIPKey = "ManualIPAddress";
+        public int SelectedIndex { get; set; } = -1;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            var manualIpAddress = localSettings.Values[kIPKey] as string;
+            if (!String.IsNullOrEmpty(manualIpAddress))
+            {
+                ManualIPEntry.Text = manualIpAddress;
+            }
+
+            mistyFinder.start();
+            ManualIPEntry.KeyUp += ManualIPEntry_KeyUp;
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            mistyFinder.stop();
+        }
+
+        private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(ManualIPEntry.Text))
+            {
+                await currentMisty.ConnectAsync(ManualIPEntry.Text, kWebSocketPort);
+            }
+            else if (SelectedIndex > -1)
+            {
+                var instance = mistyFinder.MistyInstances[SelectedIndex];
+                await currentMisty.ConnectAsync(instance.IpAddress, kWebSocketPort);
+            }
+            else
+            {
+                await new MessageDialog("Please Enter a connection string - either IP Address or Name", "Cannot connect").ShowAsync();
+            }
+        }
+
+        private async void ManualIPEntry_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                if (!String.IsNullOrEmpty(ManualIPEntry.Text))
+                {
+                    await currentMisty.ConnectAsync(ManualIPEntry.Text, kWebSocketPort);
+                }
+            }
+        }
+
+
+
+
+    }
+}
