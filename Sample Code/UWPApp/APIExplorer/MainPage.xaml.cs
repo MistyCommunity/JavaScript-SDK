628e04190a8b9a3eafa6442c77f91de8f6cc05a2
Lou Amadio <lamadio@microsoft.com>
2018-05-03 15:10:02 -0700

    Basic UWP API implemented

--- Sample Code/UWPApp/APIExplorer/MainPage.xaml.cs
+++ Sample Code/UWPApp/APIExplorer/MainPage.xaml.cs
@@ -15,6 +15,9 @@ using Windows.UI.Xaml.Navigation;
 using MistyAPI;
 using Windows.System;
 using Windows.UI.Popups;
+using System.Threading.Tasks;
+using Windows.UI;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,21 +30,31 @@ namespace APIExplorer
     {
         const int kWebSocketPort = 80;
 
-        Misty currentMisty = new Misty();
+        // API explorer only supports 1 misty at a time.
+        Misty currentMisty = null;
 
         MistyFinder mistyFinder = new MistyFinder();
         const string kIPKey = "ManualIPAddress";
         public int SelectedIndex { get; set; } = -1;
+        public int SelectedMood { get; set; } = -1;
 
         public MainPage()
         {
             this.InitializeComponent();
+            var moods = Enum.GetValues(typeof(MistyAPI.Moods)).Cast<MistyAPI.Moods>();
+            MoodList.ItemsSource = moods.ToList();
+
+            this.Loaded += MainPage_Loaded;
+            this.Unloaded += MainPage_Unloaded;
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
         {
-            base.OnNavigatedTo(e);
+            mistyFinder.stop();
+        }
 
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
             Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
             var manualIpAddress = localSettings.Values[kIPKey] as string;
             if (!String.IsNullOrEmpty(manualIpAddress))
@@ -53,28 +66,56 @@ namespace APIExplorer
             ManualIPEntry.KeyUp += ManualIPEntry_KeyUp;
         }
 
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            base.OnNavigatedFrom(e);
-
-            mistyFinder.stop();
-        }
-
         private async void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!String.IsNullOrEmpty(ManualIPEntry.Text))
+            if (currentMisty != null && currentMisty.IsConnected)
             {
-                await currentMisty.ConnectAsync(ManualIPEntry.Text, kWebSocketPort);
-            }
-            else if (SelectedIndex > -1)
-            {
-                var instance = mistyFinder.MistyInstances[SelectedIndex];
-                await currentMisty.ConnectAsync(instance.IpAddress, kWebSocketPort);
+                currentMisty.disconnect();
+                currentMisty = null;
             }
             else
             {
-                await new MessageDialog("Please Enter a connection string - either IP Address or Name", "Cannot connect").ShowAsync();
+                if (!String.IsNullOrEmpty(ManualIPEntry.Text))
+                {
+                    await ConnectToMisty(ManualIPEntry.Text);
+                }
+                else if (SelectedIndex > -1)
+                {
+                    var instance = mistyFinder.MistyInstances[SelectedIndex];
+                    await ConnectToMisty(instance.IpAddress);
+                }
+                else
+                {
+                    await new MessageDialog("Please Enter a connection string - either IP Address or Name", "Cannot connect").ShowAsync();
+                }
             }
+        }
+
+        private async Task ConnectToMisty(string ip)
+        {
+            currentMisty = new Misty();
+            currentMisty.Connected += CurrentMisty_Connected;
+            currentMisty.Disconnected += CurrentMisty_Disconnected;
+
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values[kIPKey] = ip;
+
+            await currentMisty.ConnectAsync(ip, kWebSocketPort);
+        }
+
+        private void CurrentMisty_Disconnected(Misty misty)
+        {
+            ConnectionState.Fill = new SolidColorBrush(Colors.Red);
+            currentMisty = null;
+            ConnectButton.Content = "Connect";
+        }
+
+        private void CurrentMisty_Connected(Misty misty)
+        {
+            ConnectionState.Fill = new SolidColorBrush(Colors.Green);
+            ConnectButton.Content = "Disconnect";
+
+            Debug.WriteLine("Misty Serial Number - " + currentMisty.SerialNumber);
         }
 
         private async void ManualIPEntry_KeyUp(object sender, KeyRoutedEventArgs e)
@@ -88,8 +129,19 @@ namespace APIExplorer
             }
         }
 
+        private void ConnectSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Combo Box changes should clear the manual entry
+            ManualIPEntry.Text = "";
+        }
 
-
-
+        private async void MoodSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (currentMisty != null)
+            {
+                var selectedMood = MoodList.SelectedItem;
+                await currentMisty.SetMoodAsync((Moods)selectedMood);
+            }
+        }
     }
 }
