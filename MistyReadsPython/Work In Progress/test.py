c8ce8a75ef22909a8cbd40c7ae45c1b4c9a4d670
Chris Meyer <christopher.b.meyer@gmail.com>
2019-05-17 12:21:17 -0600

    Removing Work In Progress Files
    Stupid .gitignore

--- MistyReadsPython/Work In Progress/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url     = "http://10.0.10.69/api/drive/time"
-payload = {"LinearVelocity":0,"AngularVelocity":55,"TimeMS":800}
-headers = { 'Content-Type': 'application/json'}
-res = requests.post(url, json=payload, headers=headers)
-res.raise_for_status()
