3f76d81b55e87115319a6d6bbed9cac4f1d4fb0b
Johnathan <johnathan@mistyrobotics.com>
2019-10-09 13:08:18 -0600

    Update head and arm movement commands

--- RoamAndLook/roamLook.js
+++ RoamAndLook/roamLook.js
@@ -1,10 +1,10 @@
 // On seeing a known face Misty greets and smiles... while on seeing an unknown face misty takes a picture and saves it
 
 misty.Debug("Centering Head");
-misty.MoveHeadPosition(-5, 0, 0, 100);
-misty.MoveArmPosition("left", 0, 45);
+misty.MoveHeadDegrees(0, 0, 0, 100);
+misty.MoveArmDegrees("left", 90, 45);
 misty.Pause(50);
-misty.MoveArmPosition("right", 0, 45);
+misty.MoveArmDegrees("right", 90, 45);
 
 misty.StartFaceRecognition();
 registerFaceRec();
@@ -26,23 +26,23 @@ function _FaceRec(data){
         misty.DisplayImage("Disdainful.png");
         misty.PlayAudio("013-0Bark.wav");
         misty.TakePicture(false, "Intruder", 1200, 1600, false, true);
-        misty.MoveArmPosition("left", 5, 45);
+        misty.MoveArmDegrees("left", 0, 45);
         misty.Pause(50);
-        misty.MoveArmPosition("right", 5, 45);
+        misty.MoveArmDegrees("right", 0, 45);
     } else if (faceDetected == "PUT YOUR NAME HERE") { //Modify this code block to have Misty react differently to you!
         misty.ChangeLED(0, 0, 211);
         misty.DisplayImage("Relaxed.png");
         misty.PlayAudio("001-Veep.wav");
-        misty.MoveArmPosition("left", 10, 45);
+        misty.MoveArmDegrees("left", -29, 45);
         misty.Pause(50);
-        misty.MoveArmPosition("right", 10, 45);
+        misty.MoveArmDegrees("right", -29, 45);
     } else {
         misty.ChangeLED(148, 0, 211);
         misty.DisplayImage("Wonder.png");
         misty.PlayAudio("gtcu.wav");
-        misty.MoveArmPosition("left", 10, 45);
+        misty.MoveArmDegrees("left", -29, 45);
         misty.Pause(50);
-        misty.MoveArmPosition("right", 10, 45);
+        misty.MoveArmDegrees("right", -29, 45);
     }
 
     misty.Set("FaceDetectedAt",(new Date()).toUTCString());
@@ -56,9 +56,9 @@ while (true) {
     if (misty.Get("Initiated") && secondsPast(misty.Get("FaceDetectedAt")) > 7.0){
         misty.DisplayImage("Homeostasis.png");
         misty.Set("Initiated",false);
-        misty.MoveArmPosition("left", 0, 45);
+        misty.MoveArmDegrees("left", 90, 45);
         misty.Pause(50);
-        misty.MoveArmPosition("right", 0, 45);
+        misty.MoveArmDegrees("right", 90, 45);
         misty.Drive(15,20);
         misty.Pause(1000);
         registerFaceRec();
